db5da5b9b46352a770f0e33786951447608ff738
shmilee <shmilee.zju@gmail.com>
2015-03-27 17:10:06 +0800

    path lib man

--- custom/plugins/ifts-kylin/ifts-kylin.plugin.zsh
+++ custom/plugins/ifts-kylin/ifts-kylin.plugin.zsh
@@ -1,6 +1,13 @@
 # User specific aliases and functions
 
-export PATH=$PATH:/usr/local/mpi.intel/openmpi1.6.4/bin
+_mpi=/usr/local/mpi.intel/openmpi1.6.4
+#_mpi=/usr/local/mpi.intel/mvapi2_1.8
+#_mpi=/usr/local/mpi.intel/mpich2_1.5
+
+export PATH=$_mpi/bin:$PATH
+export LD_LIBRARY_PATH=$_mpi/lib:$LD_LIBRARY_PATH
+export MANPATH=$_mpi/share/man:$MANPATH
+export INCLUDE=$_mpi/include:$INCLUDE
 
 #export PETSC_DIR=/public/home/users/tgzhang/lib/petsc-3.4.4
 #export PETSC_ARCH=linux-gnu-intel
