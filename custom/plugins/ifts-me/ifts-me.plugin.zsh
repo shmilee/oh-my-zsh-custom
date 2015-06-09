20837e6988b0fee0882b1f6aaebfaf40d50adbcb
shmilee <shmilee.zju@gmail.com>
2015-06-09 16:07:20 +0800

    ZION changes ip

--- custom/plugins/ifts-me/ifts-me.plugin.zsh
+++ custom/plugins/ifts-me/ifts-me.plugin.zsh
@@ -2,6 +2,7 @@
 alias kylin="ssh -o ServerAliveInterval=60 -p 5789 smli@10.22.92.173"
 alias oldifts="TERM=linux ssh -o ServerAliveInterval=60 -p 10321 smli@10.22.92.172"
 alias zion='ssh -o ServerAliveInterval=60 -p 5789 smli@10.22.92.171'
+alias zion2='ssh -o ServerAliveInterval=60 -p 5789 smli@192.168.5.236'
 
 scp2kylin () { scp -P 5789 -r $1 smli@10.22.92.173:upload/; }
 scp4kylin () { scp -P 5789 -r smli@10.22.92.173:$1 ./; }
