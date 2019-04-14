a7fd2394f444f3ad90af377263472bc4bdc2b017
shmilee <shmilee.zju@gmail.com>
2019-04-15 04:51:55 +0800

    update ifts-kylin2.plugin

--- custom/plugins/ifts-kylin2/ifts-kylin2.plugin.zsh
+++ custom/plugins/ifts-kylin2/ifts-kylin2.plugin.zsh
@@ -1,9 +1,4 @@
-export PATH=/public/intel/compilers_and_libraries_2018.1.148/linux/mpi/intel64/bin:$PATH
-export LD_LIBRARY_PATH=/public/intel/compilers_and_libraries_2018.1.148/linux/mpi/intel64/lib:$LD_LIBRARY_PATH
-
-source /public/intel/compilers_and_libraries_2018.1.148/linux/mkl/bin/mklvars.sh intel64
-export PATH=/public/intel/compilers_and_libraries_2018.1.148/linux/mkl/bin:$PATH
-export LD_LIBRARY_PATH=/public/intel/compilers_and_libraries_2018.1.148/linux/mkl/lib/intel64_lin:$LD_LIBRARY_PATH
+source /public/intel/compilers_and_libraries_2018.1.148/linux/pkg_bin/ifortvars.sh intel64
 
 # PETSC TODO
 export PETSC_DIR=/public/home/test3/hwyang/myapp-intel2018.1.148/petsc-3.6.4
