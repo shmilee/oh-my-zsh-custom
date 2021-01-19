224d8b1986df3f46e68e8b45c6e718a6eb2ad9ab
shmilee <shmilee.zju@gmail.com>
2021-01-19 22:55:53 +0800

    test th-3.plugin

---
+++ custom/plugins/th-3/th-3.plugin.zsh
@@ -0,0 +1,39 @@
+## test TH-3
+_software=(
+    '/vol7/software/gcc/8.2.0'
+    '/vol7/software/mpich/3.2.1-gcc8.2.0'
+    '/vol7/software/python/3.6.6-gcc8.2.0'
+    '/vol7/software/hdf5/1.8.21-gcc8.2.0-mpich3.2.1'
+)
+for _sw in ${_software[@]}; do
+    if [ -d $_sw/bin ]; then
+        export PATH=$_sw/bin:$PATH
+    fi
+    if [ -d $_sw/lib ]; then
+        export LD_LIBRARY_PATH=$_sw/lib:$LD_LIBRARY_PATH
+    fi
+    if [ -d $_sw/lib64 ]; then
+        export LD_LIBRARY_PATH=$_sw/lib64:$LD_LIBRARY_PATH
+    fi
+    if [ -d $_sw/share/man ]; then
+        export MANPATH=$_sw/share/man:$MANPATH
+    fi
+    if [ -d $_sw/include ]; then
+        export INCLUDE=$_sw/include:$INCLUDE
+    fi
+done
+unset _sw _software
+
+# User specific aliases and functions
+alias yhq='squeue'
+alias yhqc='yhq|sqc'
+alias yhi='sinfo'
+
+sqc() {
+    local user=${1:-$USER}
+    cat - | sed \
+        -e "/${user}[ \t]*R[ \t]*/ s/^.*$/$fg_bold[green]&$reset_color/" \
+        -e "/${user}[ \t]*F[ \t]*/ s/^.*$/$fg_bold[red]&$reset_color/" \
+        -e "/${user}[ \t]*PD[ \t]*/ s/^.*$/$fg_bold[yellow]&$reset_color/" \
+        -e "/${user}[ \t]*C[AG][ \t]*/ s/^.*$/$fg_bold[grey]&$reset_color/"
+}
